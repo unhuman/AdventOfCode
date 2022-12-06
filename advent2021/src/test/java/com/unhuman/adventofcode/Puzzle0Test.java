ccb791a38b411cfac51d7afc11245b360e66b353
Uman, Howard <human@cvent.com>
2022-12-06 18:24:33 -0500

    Test stuff

---
+++ advent2021/src/test/java/com/unhuman/adventofcode/Puzzle0Test.java
@@ -0,0 +1,10 @@
+package com.unhuman.adventofcode;
+
+import org.junit.jupiter.api.Test;
+
+public class Puzzle0Test {
+    @Test
+    public void test1() {
+
+    }
+}
---
+++ advent2022/src/test/java/com/unhuman/adventofcode/Day0Test.java
@@ -0,0 +1,10 @@
+package com.unhuman.adventofcode;
+
+import org.junit.jupiter.api.Test;
+
+public class Day0Test {
+    @Test
+    public void test1() {
+
+    }
+}
