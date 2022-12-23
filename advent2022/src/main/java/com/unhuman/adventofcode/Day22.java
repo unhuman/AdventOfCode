365c6d0eb1c0efaacadab75618f45a4ec8395523
Howard Uman <unhuman@gmail.com>
2022-12-23 18:44:56 -0500

    Day 22 part 2 passes tests

--- advent2022/src/main/java/com/unhuman/adventofcode/Day22.java
+++ advent2022/src/main/java/com/unhuman/adventofcode/Day22.java
@@ -12,7 +12,7 @@ public class Day22 extends InputParser {
     private static final String regex1 = "([ .#])";
     private static final String regex2 = "(\\d+)(\\w)?";
 
-    enum Facing { RIGHT, DOWN, LEFT, UP }
+    enum Facing {RIGHT, DOWN, LEFT, UP}
 
     public Day22(String[] filenameAndCookieInfo) {
         super(filenameAndCookieInfo, regex1, regex2);
@@ -38,7 +38,7 @@ public class Day22 extends InputParser {
                 char character = (elementIdx < line.size()) ? line.get(elementIdx).charAt(0) : ' ';
                 map[lineIdx][elementIdx] = character;
                 if (currentLocation == null && character != ' ') {
-                    currentLocation = new Point( elementIdx, lineIdx);
+                    currentLocation = new Point(elementIdx, lineIdx);
                 }
             }
         }
@@ -79,6 +79,7 @@ public class Day22 extends InputParser {
 
     /**
      * Returns true if we did advance, false if otherwise.
+     *
      * @param map
      * @param currentLocation
      * @param facing
@@ -87,7 +88,7 @@ public class Day22 extends InputParser {
     boolean advance(char[][] map, Point currentLocation, Facing facing) {
         int xIncrement = 0;
         int yIncrement = 0;
-        switch(facing) {
+        switch (facing) {
             case RIGHT:
                 xIncrement = 1;
                 break;
@@ -154,7 +155,7 @@ public class Day22 extends InputParser {
                 char character = (elementIdx < line.size()) ? line.get(elementIdx).charAt(0) : ' ';
                 map[lineIdx][elementIdx] = character;
                 if (currentLocation == null && character != ' ') {
-                    currentLocation = new Point( elementIdx, lineIdx);
+                    currentLocation = new Point(elementIdx, lineIdx);
                 }
             }
         }
@@ -195,59 +196,50 @@ public class Day22 extends InputParser {
 
     /**
      * Returns true if we did advance, false if otherwise.
+     *
      * @param map
      * @param currentLocation
      * @param facing
      * @return
      */
     boolean advance2(char[][] map, Point currentLocation, AtomicReference<Facing> facing) {
-        // Both the example and the real data have a 3/4 arrangement of cubes.
-        // So, we can use that to figure out the size of each side
         int cubeSize = Math.abs(map.length - map[0].length);
-
-        int xIncrement = 0;
-        int yIncrement = 0;
-        switch(facing.get()) {
-            case RIGHT:
-                xIncrement = 1;
-                break;
-            case DOWN:
-                yIncrement = 1;
-                break;
-            case LEFT:
-                xIncrement = -1;
-                break;
-            case UP:
-                yIncrement = -1;
-                break;
-        }
-
-        Point checkPoint = new Point(currentLocation);
         while (true) {
+            Point checkPoint = new Point(currentLocation);
+            AtomicReference<Facing> checkFacing = new AtomicReference<>(facing.get());
+            int xIncrement = 0;
+            int yIncrement = 0;
+            switch (checkFacing.get()) {
+                case RIGHT:
+                    xIncrement = 1;
+                    break;
+                case DOWN:
+                    yIncrement = 1;
+                    break;
+                case LEFT:
+                    xIncrement = -1;
+                    break;
+                case UP:
+                    yIncrement = -1;
+                    break;
+            }
+
             checkPoint = new Point(checkPoint.x + xIncrement, checkPoint.y + yIncrement);
 
-            // TODO: Adjust how we determine edges of the cube and teleport and adjust facing
-
-            if (checkPoint.x < 0) {
-                checkPoint.x = map[0].length - 1;
-            }
-            if (checkPoint.x == map[0].length) {
-                checkPoint.x = 0;
-            }
-            if (checkPoint.y < 0) {
-                checkPoint.y = map.length - 1;
-            }
-            if (checkPoint.y == map.length) {
-                checkPoint.y = 0;
-            }
-
-            // Check if we are out of bounds and teleport to another place
-            if (map[checkPoint.y][checkPoint.x] == ' ') {
-
+            if (checkPoint.x < 0 || checkPoint.x == map[0].length
+                    || checkPoint.y < 0 || checkPoint.y == map.length
+                    || map[checkPoint.y][checkPoint.x] == ' ') {
+                if (map.length < 100) {
+                    handleTeleportForTest(cubeSize, checkPoint, checkFacing);
+                } else {
+                    handleTeleportForProblem(cubeSize, checkPoint, checkFacing);
+                }
             }
 
             if (map[checkPoint.y][checkPoint.x] == '.') {
                 currentLocation.setLocation(checkPoint);
+                facing.set(checkFacing.get());
+                System.out.println("Plotting: " + checkPoint.x + " / " + checkPoint.y + " Direction: " + facing.get());
                 return true;
             }
 
@@ -257,5 +249,109 @@ public class Day22 extends InputParser {
 
             // next iteration will find something
         }
+    }
+
+    void handleTeleportForTest(int cubeSize, Point checkPoint, AtomicReference<Facing> facing) {
+
+        // Cube 3 (top) - move left to 6
+        if (checkPoint.y >= 0 && checkPoint.y < cubeSize && checkPoint.x == cubeSize - 1 && facing.get() == Facing.LEFT) {
+            facing.set(Facing.DOWN);
+            checkPoint.x = cubeSize + checkPoint.y;
+            checkPoint.y = cubeSize;
+        }
+        // Cube 3 (top) - move right to 12
+        if (checkPoint.y >= 0 && checkPoint.y < cubeSize && checkPoint.x == cubeSize * 3 && facing.get() == Facing.RIGHT) {
+            facing.set(Facing.LEFT);
+            checkPoint.x = cubeSize * 3 - 1;
+            checkPoint.y = cubeSize * 3 - 1 - checkPoint.y;
+        }
+        // Cube 3 (top) - move up to 5
+        if (checkPoint.y < 0 && facing.get() == Facing.UP) {
+            facing.set(Facing.DOWN);
+            checkPoint.x = cubeSize * 3 - checkPoint.x;
+            checkPoint.y = cubeSize;
+        }
+
+        // Cube 5 - left to 12
+        if (checkPoint.x < 0 && facing.get() == Facing.LEFT) {
+            facing.set(Facing.UP);
+            checkPoint.x = cubeSize * 4 - 1 - checkPoint.y;
+            checkPoint.y = cubeSize * 3 - 1;
+        }
+        // Cube 5 - move up to 3
+        if (checkPoint.x < cubeSize && checkPoint.y < cubeSize && facing.get() == Facing.UP) {
+            facing.set(Facing.DOWN);
+            checkPoint.x = cubeSize * 3 - 1 - checkPoint.x;
+            checkPoint.y = 0;
+        }
+        // Cube 5 - move down to 11
+        if (checkPoint.y == cubeSize * 2 && facing.get() == Facing.DOWN) {
+            facing.set(Facing.UP);
+            checkPoint.x = cubeSize * 3 - 1 - checkPoint.x;
+            checkPoint.y = cubeSize * 3 - 1;
+        }
+
+        // Cube 6 - move up to 3
+        if (checkPoint.x >= cubeSize && checkPoint.x < cubeSize * 2 && checkPoint.y < cubeSize && facing.get() == Facing.UP) {
+            facing.set(Facing.RIGHT);
+            checkPoint.y = checkPoint.x - cubeSize;
+            checkPoint.x = cubeSize * 2;
+        }
+        // Cube 6 - move down to 11
+        if (checkPoint.x >= cubeSize && checkPoint.x < cubeSize * 2 && checkPoint.y == cubeSize * 2 && facing.get() == Facing.DOWN) {
+            facing.set(Facing.RIGHT);
+            checkPoint.y = cubeSize * 3 - 1 - checkPoint.x;
+            checkPoint.x = cubeSize * 2;
+        }
+
+        // Cube 6 - move down to 11
+        if (checkPoint.x >= cubeSize && checkPoint.x < cubeSize * 2 && checkPoint.y == cubeSize * 2 && facing.get() == Facing.DOWN) {
+            facing.set(Facing.RIGHT);
+            checkPoint.y = cubeSize * 3 - 1 - checkPoint.x;
+            checkPoint.x = cubeSize * 2;
+        }
+
+        // Cube 7 - move right to 12
+        if (checkPoint.x == cubeSize * 3&& checkPoint.y >= cubeSize && checkPoint.y < cubeSize * 2 && facing.get() == Facing.RIGHT) {
+            facing.set(Facing.DOWN);
+            checkPoint.x = cubeSize * 3 + (cubeSize * 2 - 1 - checkPoint.y);
+            checkPoint.y = cubeSize * 2;
+        }
+
+        // Cube 11 (bottom) - left to 6
+        if (checkPoint.y >= cubeSize * 2 && checkPoint.x == cubeSize * 2 - 1 && facing.get() == Facing.LEFT) {
+            facing.set(Facing.UP);
+            checkPoint.x = cubeSize * 2 - 1 - (checkPoint.y - cubeSize * 2);
+            checkPoint.y = cubeSize * 2 - 1;
+        }
+
+        // Cube 11 (bottom) - down to 5
+        if (checkPoint.y == cubeSize * 3 && checkPoint.x >= cubeSize * 2 && checkPoint.x < cubeSize * 3 && facing.get() == Facing.DOWN) {
+            facing.set(Facing.UP);
+            checkPoint.x = cubeSize * 3 - 1 - checkPoint.x;
+            checkPoint.y = cubeSize * 2 - 1;
+        }
+
+        // Cube 12 - move up to 7
+        if (checkPoint.x >= cubeSize * 3 && checkPoint.x < cubeSize * 4 && checkPoint.y == cubeSize * 2 - 1 && facing.get() == Facing.UP) {
+            facing.set(Facing.LEFT);
+            checkPoint.y = cubeSize * 2 - 1 - (cubeSize * 3 - 1 - checkPoint.x);
+            checkPoint.x = cubeSize * 3 - 1;
+        }
+        // Cube 12 - move down to 5
+        if (checkPoint.y == cubeSize * 3 && checkPoint.x >= cubeSize * 3 && checkPoint.x < cubeSize * 4 && facing.get() == Facing.UP) {
+            facing.set(Facing.RIGHT);
+            checkPoint.y = cubeSize + (cubeSize * 4 - 1 - checkPoint.x);
+            checkPoint.x = 0;
+        }
+        // Cube 12 - move right to 3
+        if (checkPoint.x == cubeSize * 4 && checkPoint.y >= cubeSize * 2 && facing.get() == Facing.RIGHT) {
+            facing.set(Facing.LEFT);
+            checkPoint.y = cubeSize * 3 - 1 - checkPoint.y;
+            checkPoint.x = cubeSize * 3 - 1;
+        }
+    }
+
+    void handleTeleportForProblem(int cubeSize, Point checkPoint, AtomicReference<Facing> facing) {
     }
 }
