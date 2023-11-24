41066c7865776b4637e8f297602528cd5dcb2367
Howard Uman <unhuman@gmail.com>
2023-11-24 16:47:04 -0500

    Prep 2023

---
+++ advent2021/src/test/java/com/unhuman/adventofcode2021/Day0Test.java
@@ -0,0 +1,15 @@
+package com.unhuman.adventofcode2021;
+
+import org.junit.jupiter.api.Test;
+
+public class Day0Test {
+    @Test
+    public void test1() {
+//        String filename = "src/test/resources/DayXCase.txt";
+//        DayX day = new DayX(filename);
+//        ConfigGroup[] groups = day.parseFiles();
+//
+//        Assertions.assertEquals(X, day.processInput1(groups[0], groups[1]));
+//        Assertions.assertEquals(Y, day.processInput2(groups[0], groups[1]));
+    }
+}
---
+++ advent2023/src/main/java/com/unhuman/adventofcode2023/Advent2023.java
@@ -0,0 +1,13 @@
+package com.unhuman.adventofcode2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class Advent2023
+{
+    public static void main( String[] args )
+    {
+        new Day1().process();
+    }
+}
