b413acef71fe0fed9df7b8f8d34155a15e7fe0a4
Howard Uman <unhuman@gmail.com>
2023-12-16 14:28:12 -0500

    2023 Day 16

--- advent2023/src/main/java/com/unhuman/adventofcode2023/Day16.java
+++ advent2023/src/main/java/com/unhuman/adventofcode2023/Day16.java
@@ -2,12 +2,65 @@ package com.unhuman.adventofcode2023;
 
 import com.unhuman.adventofcode.aoc_framework.InputParser;
 import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
-import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
-import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
+import com.unhuman.adventofcode.aoc_framework.utility.Matrix;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Day16 extends InputParser {
-    private static final String regex1 = null;
+    private static final String regex1 = "(.)";
     private static final String regex2 = null;
+
+    enum Direction { UP, DOWN, LEFT, RIGHT }
+
+    static class Beam {
+        Point point;
+        Direction direction;
+
+        public Beam(Beam otherBeam) {
+            this.point = new Point(otherBeam.point);
+            this.direction = otherBeam.direction;
+        }
+
+        public Beam(int x, int y, Direction direction) {
+            this.point = new java.awt.Point(x, y);
+            this.direction = direction;
+        }
+
+        public void move() {
+            switch (direction) {
+                case UP: this.point.y--; break;
+                case DOWN: this.point.y++; break;
+                case LEFT: this.point.x--; break;
+                case RIGHT: this.point.x++; break;
+            }
+        }
+
+        public Point getPoint() {
+            return point;
+        }
+
+        public Direction getDirection() {
+            return direction;
+        }
+
+        public void setDirection(Direction direction) {
+            this.direction = direction;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Beam other = (Beam) obj;
+            return (this.direction == other.direction && this.point.equals(other.point));
+        }
+
+        @Override
+        public int hashCode() {
+            return this.point.hashCode() * 17 + this.direction.hashCode();
+        }
+    }
 
     public Day16() {
         super(2023, 16, regex1, regex2);
@@ -19,19 +72,127 @@ public class Day16 extends InputParser {
 
     @Override
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        for (GroupItem item : configGroup) {
-            for (ItemLine line : item) {
-                for (String element : line) {
+        Matrix matrix = new Matrix(configGroup);
 
+        Beam startingBeam = new Beam(-1, 0, Direction.RIGHT);
+
+        return navigateField(matrix, startingBeam);
+    }
+
+    private int navigateField(Matrix matrix, Beam startingBeam) {
+        java.util.List<Beam> beams = new ArrayList<>();
+        beams.add(startingBeam);
+        Set<Point> energized = new HashSet<>();
+        Set<Beam> energizedDirections = new HashSet<>();
+        while (beams.size() > 0) {
+            for (int beamNum = beams.size() - 1; beamNum >= 0; beamNum--) {
+                Beam beam = beams.get(beamNum);
+                beam.move();
+
+                // We are done if this is off matrix or re-energizes in the same direction
+                if (!isValid(matrix, beam)
+                        || energizedDirections.contains(beam)) {
+                    beams.remove(beamNum);
+                    continue;
+                }
+                energized.add(new Point(beam.getPoint()));
+                energizedDirections.add(new Beam(beam));
+
+                char place = matrix.getPointValue(beam.getPoint());
+                Direction dir = beam.getDirection();
+
+                // Handle just movement
+                if (place == '.') {
+                    continue;
+                }
+
+                // handle splits
+                if (place == '-') {
+                    if (dir == Direction.UP || dir == Direction.DOWN) {
+                        beam.setDirection(Direction.RIGHT);
+                        Beam beam2 = new Beam(beam);
+                        beam2.setDirection(Direction.LEFT);
+                        beams.add(beam2);
+                    }
+                    continue;
+                }
+
+                if (place == '|') {
+                    if (dir == Direction.LEFT || dir == Direction.RIGHT) {
+                        beam.setDirection(Direction.UP);
+                        Beam beam2 = new Beam(beam);
+                        beam2.setDirection(Direction.DOWN);
+                        beams.add(beam2);
+                    }
+                    continue;
+                }
+
+                switch (place) {
+                    case '/':
+                        switch (dir) {
+                            case UP:
+                                beam.setDirection(Direction.RIGHT);
+                                break;
+                            case DOWN:
+                                beam.setDirection(Direction.LEFT);
+                                break;
+                            case LEFT:
+                                beam.setDirection(Direction.DOWN);
+                                break;
+                            case RIGHT:
+                                beam.setDirection(Direction.UP);
+                                break;
+                        }
+                        break;
+                    case '\\':
+                        switch (dir) {
+                            case UP:
+                                beam.setDirection(Direction.LEFT);
+                                break;
+                            case DOWN:
+                                beam.setDirection(Direction.RIGHT);
+                                break;
+                            case LEFT:
+                                beam.setDirection(Direction.UP);
+                                break;
+                            case RIGHT:
+                                beam.setDirection(Direction.DOWN);
+                                break;
+                        }
+                        break;
                 }
             }
         }
 
-        return 1;
+        return energized.size();
+    }
+
+    boolean isValid(Matrix matrix, Beam beam) {
+        Point point = beam.getPoint();
+        return (point.x >= 0 && point.y >= 0 && point.x < matrix.getWidth() && point.y < matrix.getHeight());
     }
 
     @Override
     public Object processInput2(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        return 2;
+        Matrix matrix = new Matrix(configGroup);
+
+        int maxScore = 0;
+        for (int i = 0; i < matrix.getWidth(); i++) {
+            Beam startingBeam = new Beam(i, -1, Direction.DOWN);
+            maxScore = Math.max(maxScore, navigateField(matrix, startingBeam));
+
+            Beam startingBeam2 = new Beam(i, matrix.getHeight(), Direction.UP);
+            maxScore = Math.max(maxScore, navigateField(matrix, startingBeam2));
+        }
+
+        for (int i = 0; i < matrix.getHeight(); i++) {
+            Beam startingBeam = new Beam(-1, i, Direction.RIGHT);
+            maxScore = Math.max(maxScore, navigateField(matrix, startingBeam));
+
+            Beam startingBeam2 = new Beam(matrix.getWidth(), i, Direction.LEFT);
+            maxScore = Math.max(maxScore, navigateField(matrix, startingBeam2));
+        }
+
+        return maxScore;
     }
 }
--- advent2023/src/test/java/com/unhuman/adventofcode2023/Day17Test.java
+++ advent2023/src/test/java/com/unhuman/adventofcode2023/Day17Test.java
@@ -5,34 +5,25 @@ import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Day16Test {
+public class Day17Test {
     // data must be at least 2 lines - add \n for single line data
     private static final String DATA =
-                    ".|...\\....\n" +
-                    "|.-.\\.....\n" +
-                    ".....|-...\n" +
-                    "........|.\n" +
-                    "..........\n" +
-                    ".........\\\n" +
-                    "..../.\\\\..\n" +
-                    ".-.-/..|..\n" +
-                    ".|....-|.\\\n" +
-                    "..//.|....";
+            "";
 
     static InputParser day;
     static {
-        day = new Day16(DATA);
+        day = new Day17(DATA);
     }
 
     @Test
     public void test1() {
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(46, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(0, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
     public void test2() {
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(51, day.processInput2(groups[0], groups[1]));
+        Assertions.assertEquals(0, day.processInput2(groups[0], groups[1]));
     }
 }
