5fe6c45a5d8686f522b48f15330750a6ec52488b
Howard Uman <unhuman@gmail.com>
2024-12-02 19:57:52 -0500

    2024 day 2, prep day 3

--- advent2024/src/main/java/com/unhuman/adventofcode2024/Day2.java
+++ advent2024/src/main/java/com/unhuman/adventofcode2024/Day2.java
@@ -5,8 +5,16 @@ import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
 import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day2 extends InputParser {
-    private static final String regex1 = null;
+    enum RateType {
+        UNKNOWN,
+        INCREASING,
+        DECREASING
+    }
+    private static final String regex1 = "(\\d+)\\s*";
     private static final String regex2 = null;
 
     public Day2() {
@@ -21,17 +29,71 @@ public class Day2 extends InputParser {
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
         // easier to assume there's only one group
         GroupItem item = configGroup.get(0);
-        for (ItemLine line : item) {
-            for (String element : line) {
 
+        int count = 0;
+        for (ItemLine line : item) {
+            List<Integer> report = new ArrayList<>();
+            for (String element : line) {
+                report.add(Integer.parseInt(element));
+            }
+            if (isSafe(report)) {
+                count += 1;
             }
         }
 
-        return 1;
+        return count;
     }
+
+    private boolean isSafe(List<Integer> report) {
+        int current = report.get(0);
+        RateType rateType = RateType.UNKNOWN;
+        for (int i = 1; i < report.size(); i++) {
+            int next = report.get(i);
+            int diff = Math.abs(next - current);
+            if (diff <= 0 || diff > 3) {
+                return false;
+            }
+            RateType nextRateType = (next > current) ? RateType.INCREASING : RateType.DECREASING;
+            if (rateType != RateType.UNKNOWN && nextRateType != rateType) {
+                return false;
+            }
+            rateType = nextRateType;
+            current = next;
+        }
+        return true;
+    }
+
+    private boolean isSafe2(List<Integer> report) {
+        if (isSafe(report)) {
+            return true;
+        }
+        for (int i = 0; i < report.size(); i++) {
+            List<Integer> reportClone = new ArrayList<>(report);
+            reportClone.remove(i);
+            if (isSafe(reportClone)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public Object processInput2(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        return 2;
+        // easier to assume there's only one group
+        GroupItem item = configGroup.get(0);
+
+        int count = 0;
+        for (ItemLine line : item) {
+            List<Integer> report = new ArrayList<>();
+            for (String element : line) {
+                report.add(Integer.parseInt(element));
+            }
+            if (isSafe2(report)) {
+                count += 1;
+            }
+        }
+
+        return count;
     }
 }
--- advent2024/src/test/java/com/unhuman/adventofcode2024/Day3Test.java
+++ advent2024/src/test/java/com/unhuman/adventofcode2024/Day3Test.java
@@ -5,18 +5,14 @@ import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class Day2Test {
+public class Day3Test {
     // data must be at least 2 lines - add \n for single line data
     private static final String DATA =
-            "7 6 4 2 1\n" +
-            "1 2 7 8 9\n" +
-            "9 7 6 2 1\n" +
-            "1 3 2 4 5\n" +
-            "8 6 4 4 1\n" +
-            "1 3 6 7 9";
+            "";
 
     static InputParser getDay(String data) {
-        return new Day2(data);
+        return null;
+        //return new DayX(data);
     }
 
     @Test
@@ -24,7 +20,7 @@ public class Day2Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(2, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(0, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
@@ -32,6 +28,6 @@ public class Day2Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(4, day.processInput2(groups[0], groups[1]));
+        Assertions.assertEquals(0, day.processInput2(groups[0], groups[1]));
     }
 }
