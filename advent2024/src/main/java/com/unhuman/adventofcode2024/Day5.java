c2624c0d1af9356523c9c7ada0f6ee6c105026ca
Uman, Howard <human@cvent.com>
2024-12-05 15:03:21 -0500

    2024 day 5, prep day 6

--- advent2024/src/main/java/com/unhuman/adventofcode2024/Day5.java
+++ advent2024/src/main/java/com/unhuman/adventofcode2024/Day5.java
@@ -5,9 +5,15 @@ import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
 import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Day5 extends InputParser {
-    private static final String regex1 = null;
-    private static final String regex2 = null;
+    private static final String regex1 = "(\\d+)\\|(\\d+)";
+    private static final String regex2 = "(\\d+)";
 
     public Day5() {
         super(2024, 5, regex1, regex2);
@@ -17,23 +23,140 @@ public class Day5 extends InputParser {
         super(filename, regex1, regex2);
     }
 
+    // map of page number to pages that must come before it (if both exist).
+    private class ConditionalPageRules extends HashMap<Long, Set<Long>> { }
+
     @Override
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
+        // For a given page, what pages must come before it
+        ConditionalPageRules pageRules = new ConditionalPageRules();
+
+        long total = 0;
+
         // easier to assume there's only one group
         GroupItem item = configGroup.get(0);
         for (ItemLine line : item) {
-            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-//                char value = line.getChar(itemNum);
-//                String value = line.getString(itemNum);
-//                Long value = line.getLong(itemNum);
+            Long pageNum = line.getLong(0);
+            Long priorPage = line.getLong(1);
+            if (!pageRules.containsKey(priorPage)) {
+                pageRules.put(priorPage, new HashSet<>(List.of(pageNum)));
+            } else {
+                pageRules.get(priorPage).add(pageNum);
             }
         }
 
-        return 1;
+        item = configGroup1.get(0);
+        for (ItemLine line : item) {
+            List<Long> pagesToPrint = new ArrayList<>();
+            for (int i = 0; i< line.size(); i++) {
+                pagesToPrint.add(line.getLong(i));
+            }
+            if (isValidOrdering(pagesToPrint, pageRules)) {
+                total += pagesToPrint.get((pagesToPrint.size() - 1) / 2);
+            }
+        }
+
+        return total;
+    }
+
+    boolean isValidOrdering(List<Long> pagesToPrint, ConditionalPageRules pageRules) {
+        HashSet<Long> printingPages = new HashSet<>(pagesToPrint);
+        HashSet<Long> printedPages = new HashSet<>();
+        for (int i = 0; i < pagesToPrint.size(); i++) {
+            long pageNum = pagesToPrint.get(i);
+
+            Set<Long> checkPageNums = pageRules.get(pageNum);
+
+            if (checkPageNums != null) {
+                for (Long checkPage : checkPageNums) {
+                    if (printingPages.contains(checkPage)) {
+                        if (!printedPages.contains(checkPage)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            // track
+            printedPages.add(pageNum);
+        }
+
+        return true;
+    }
+
+    /** This will return true if it can mutate the pagesToPrint to be in a valid order */
+    boolean fixValidOrdering(List<Long> pagesToPrint, ConditionalPageRules pageRules) {
+        HashSet<Long> printingPages = new HashSet<>(pagesToPrint);
+
+        while (true) {
+            boolean modified = false;
+            HashSet<Long> printedPages = new HashSet<>();
+            for (int i = 0; i < pagesToPrint.size(); i++) {
+                long pageNum = pagesToPrint.get(i);
+
+                Set<Long> checkPageNums = pageRules.get(pageNum);
+
+                if (checkPageNums != null) {
+                    for (Long checkPage : checkPageNums) {
+                        if (printingPages.contains(checkPage)) {
+                            if (!printedPages.contains(checkPage)) {
+                                // remove the checkPage from the list
+                                pagesToPrint.remove(checkPage);
+                                // add it back right before where we are
+                                pagesToPrint.add(i, checkPage);
+
+                                if (isValidOrdering(pagesToPrint, pageRules)) {
+                                    return true;
+                                }
+                                modified = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (modified) {
+                        break;
+                    }
+                }
+                // track
+                printedPages.add(pageNum);
+            }
+        }
     }
 
     @Override
     public Object processInput2(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        return 2;
+        // For a given page, what pages must come before it
+        ConditionalPageRules pageRules = new ConditionalPageRules();
+
+        long total = 0;
+
+        // easier to assume there's only one group
+        GroupItem item = configGroup.get(0);
+        for (ItemLine line : item) {
+            Long pageNum = line.getLong(0);
+            Long priorPage = line.getLong(1);
+            if (!pageRules.containsKey(priorPage)) {
+                pageRules.put(priorPage, new HashSet<>(List.of(pageNum)));
+            } else {
+                pageRules.get(priorPage).add(pageNum);
+            }
+        }
+
+        item = configGroup1.get(0);
+        for (ItemLine line : item) {
+            List<Long> pagesToPrint = new ArrayList<>();
+            for (int i = 0; i< line.size(); i++) {
+                pagesToPrint.add(line.getLong(i));
+            }
+            // compared to part 1 - we want the lines that are invalidly ordered
+            // and we want to fix the ordering
+            if (!isValidOrdering(pagesToPrint, pageRules)) {
+                if (fixValidOrdering(pagesToPrint, pageRules)) {
+                    total += pagesToPrint.get((pagesToPrint.size() - 1) / 2);
+                }
+            }
+        }
+
+        return total;
     }
 }
--- advent2024/src/test/java/com/unhuman/adventofcode2024/Day5Test.java
+++ advent2024/src/test/java/com/unhuman/adventofcode2024/Day5Test.java
@@ -8,7 +8,34 @@ import org.junit.jupiter.api.Test;
 public class Day5Test {
     // data must be at least 2 lines - add \n for single line data
     private static final String DATA =
-            "";
+            "47|53\n" +
+                    "97|13\n" +
+                    "97|61\n" +
+                    "97|47\n" +
+                    "75|29\n" +
+                    "61|13\n" +
+                    "75|53\n" +
+                    "29|13\n" +
+                    "97|29\n" +
+                    "53|29\n" +
+                    "61|53\n" +
+                    "97|53\n" +
+                    "61|29\n" +
+                    "47|13\n" +
+                    "75|47\n" +
+                    "97|75\n" +
+                    "47|61\n" +
+                    "75|61\n" +
+                    "47|29\n" +
+                    "75|13\n" +
+                    "53|13\n" +
+                    "\n" +
+                    "75,47,61,53,29\n" +
+                    "97,61,53,29,13\n" +
+                    "75,29,13\n" +
+                    "75,97,47,61,53\n" +
+                    "61,13,29\n" +
+                    "97,13,75,29,47";
 
     static InputParser getDay(String data) {
         return new Day5(data);
@@ -19,7 +46,7 @@ public class Day5Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(143L, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
@@ -27,6 +54,6 @@ public class Day5Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(0L, day.processInput2(groups[0], groups[1]));
+        Assertions.assertEquals(123L, day.processInput2(groups[0], groups[1]));
     }
 }
