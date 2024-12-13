4cb6138417a3463a311a336f1baa18e706f70cd5
Uman, Howard <human@cvent.com>
2024-12-13 15:05:26 -0500

    2024 day 13 pt 1

--- advent2024/src/main/java/com/unhuman/adventofcode2024/Day13.java
+++ advent2024/src/main/java/com/unhuman/adventofcode2024/Day13.java
@@ -6,8 +6,11 @@ import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
 import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
 
 public class Day13 extends InputParser {
-    private static final String regex1 = null;
+    private static final String regex1 = "(Button|Prize)\\s*([AB]?): X[+=](\\d+), Y[+=](\\d+)";
     private static final String regex2 = null;
+
+    private static final long BUTTON_A_COST = 3;
+    private static final long BUTTON_B_COST = 1;
 
     public Day13() {
         super(2024, 13, regex1, regex2);
@@ -19,32 +22,124 @@ public class Day13 extends InputParser {
 
     @Override
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        // easier to assume there's only one group
-        GroupItem group0 = configGroup.get(0);
-        for (ItemLine line : group0) {
-            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-//                char value = line.getChar(itemNum);
-//                String value = line.getString(itemNum);
-//                Long value = line.getLong(itemNum);
+        Long totalMinimumCost = 0L;
+        for (GroupItem group : configGroup) {
+            ItemLine line = group.get(0);
+            assert ("Button".equals(line.getString(0)));
+            Button button1 = new Button(line.getChar(1), line.getLong(2), line.getLong(3));
+
+            line = group.get(1);
+            assert ("Button".equals(line.getString(0)));
+            Button button2 = new Button(line.getChar(1), line.getLong(2), line.getLong(3));
+
+            line = group.get(2);
+            assert ("Prize".equals(line.getString(0)));
+            Prize prize = new Prize(line.getLong(2), line.getLong(3));
+
+            try {
+                totalMinimumCost += calculateMinimumCost(button1, button2, prize);
+            } catch (Exception e) {
+                // ignore
             }
         }
-
-        // Here's code for a 2nd group, if needed
-//        GroupItem group1 = configGroup1.get(0);
-//        for (ItemLine line : group1) {
-//            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-////                char value = line.getChar(itemNum);
-////                String value = line.getString(itemNum);
-////                Long value = line.getLong(itemNum);
-//            }
-//        }
-
-
-        return 1;
+        return totalMinimumCost;
     }
+
+    long calculateMinimumCost(Button buttonA, Button buttonB, Prize prize) {
+        Long minValue = null;
+        long buttonAMax1 = prize.xLocation / buttonA.xDelta;
+        long buttonAMax2 = prize.yLocation / buttonA.yDelta;
+        long buttonAMax = Math.min(buttonAMax1, buttonAMax2);
+
+        for (long i = 0; i <= buttonAMax; i++) {
+            long xLocation = prize.xLocation - i * buttonA.xDelta;
+            long yLocation = prize.yLocation - i * buttonA.yDelta;
+            if (xLocation >= 0 && yLocation >= 0) {
+                long buttonBMax1 = xLocation / buttonB.xDelta;
+                long buttonBMax2 = yLocation / buttonB.yDelta;
+                long buttonBMax = Math.min(buttonBMax1, buttonBMax2);
+
+                if (i * buttonA.xDelta + buttonBMax * buttonB.xDelta == prize.xLocation &&
+                    i * buttonA.yDelta + buttonBMax * buttonB.yDelta == prize.yLocation) {
+                    long cost = i * BUTTON_A_COST + buttonBMax * BUTTON_B_COST;
+                    if (minValue == null || cost < minValue) {
+                        minValue = cost;
+                    }
+                }
+            }
+        }
+        if (minValue != null) {
+            return minValue;
+        } else {
+            throw new RuntimeException("No solution found");
+        }
+    }
+
+    // This is expensive.  I'm sure there's a better way to do this.
+//    long calculateMinimumCost(Button buttonA, Button buttonB, Prize prize) {
+//        if (prize.xLocation < 0 || prize.yLocation < 0) {
+//            throw new RuntimeException("No solution found");
+//        }
+//
+//        if (buttonA.xDelta == prize.xLocation && buttonA.yDelta == prize.yLocation) {
+//            return BUTTON_A_COST;
+//        }
+//        if (buttonB.xDelta == prize.xLocation && buttonB.yDelta == prize.yLocation) {
+//            return BUTTON_B_COST;
+//        }
+//
+//        int caught = 0;
+//        Long costA = Long.MAX_VALUE;
+//        Long costB = Long.MAX_VALUE;
+//        try {
+//            costA = BUTTON_A_COST + calculateMinimumCost(buttonA, buttonB,
+//                    new Prize(prize.xLocation - buttonA.xDelta, prize.yLocation - buttonA.yDelta));
+//        } catch (Exception e) {
+//            caught++;
+//        }
+//        try {
+//            costB = BUTTON_B_COST + calculateMinimumCost(buttonA, buttonB,
+//                    new Prize(prize.xLocation - buttonB.xDelta, prize.yLocation - buttonB.yDelta));
+//        } catch (Exception e) {
+//            caught++;
+//        }
+//
+//        if (caught == 2) {
+//            throw new RuntimeException("No solution found");
+//        }
+//
+//        return Math.min(costA, costB);
+//    }
 
     @Override
     public Object processInput2(ConfigGroup configGroup, ConfigGroup configGroup1) {
-        return 2;
+        Long totalMinimumCost = 0L;
+        final long ENGLARGER = 10000000000000L;
+        for (GroupItem group : configGroup) {
+            ItemLine line = group.get(0);
+            assert ("Button".equals(line.getString(0)));
+            Button button1 = new Button(line.getChar(1), line.getLong(2), line.getLong(3));
+
+            line = group.get(1);
+            assert ("Button".equals(line.getString(0)));
+            Button button2 = new Button(line.getChar(1), line.getLong(2), line.getLong(3));
+
+            line = group.get(2);
+            assert ("Prize".equals(line.getString(0)));
+            Prize prize = new Prize(line.getLong(2) + ENGLARGER, line.getLong(3) + ENGLARGER);
+
+            try {
+                System.out.print("Next");
+                totalMinimumCost += calculateMinimumCost(button1, button2, prize);
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+        return totalMinimumCost;
+    }
+
+    record Prize(long xLocation, long yLocation) {
+    }
+    record Button(char name, long xDelta, long yDelta) {
     }
 }
--- advent2024/src/test/java/com/unhuman/adventofcode2024/Day13Test.java
+++ advent2024/src/test/java/com/unhuman/adventofcode2024/Day13Test.java
@@ -8,10 +8,124 @@ import org.junit.jupiter.api.Test;
 public class Day13Test {
     // data must be at least 2 lines - add \n for single line data
     private static final String DATA =
-            "\n";
+            "Button A: X+94, Y+34\n" +
+            "Button B: X+22, Y+67\n" +
+            "Prize: X=8400, Y=5400\n" +
+            "\n" +
+            "Button A: X+26, Y+66\n" +
+            "Button B: X+67, Y+21\n" +
+            "Prize: X=12748, Y=12176\n" +
+            "\n" +
+            "Button A: X+17, Y+86\n" +
+            "Button B: X+84, Y+37\n" +
+            "Prize: X=7870, Y=6450\n" +
+            "\n" +
+            "Button A: X+69, Y+23\n" +
+            "Button B: X+27, Y+71\n" +
+            "Prize: X=18641, Y=10279\n";
 
     static InputParser getDay(String data) {
         return new Day13(data);
+    }
+
+    @Test
+    public void test1pre0() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=1, Y=1\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1pre1() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=94, Y=34\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(3L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1pre2() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=22, Y=67\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(1L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1pre3() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=116, Y=101\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(4L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1pre4() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=117, Y=102\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1a() {
+        String data = "Button A: X+94, Y+34\n" +
+                "Button B: X+22, Y+67\n" +
+                "Prize: X=8400, Y=5400\n";
+
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(280L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1b() {
+        String data =
+                "Button A: X+26, Y+66\n" +
+                        "Button B: X+67, Y+21\n" +
+                        "Prize: X=12748, Y=12176\n";
+
+                        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1c() {
+        String data =
+                "Button A: X+17, Y+86\n" +
+                        "Button B: X+84, Y+37\n" +
+                        "Prize: X=7870, Y=6450\n";
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(200L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test1d() {
+        String data =
+                "Button A: X+69, Y+23\n" +
+                        "Button B: X+27, Y+71\n" +
+                        "Prize: X=18641, Y=10279\n";
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
@@ -19,7 +133,7 @@ public class Day13Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(480L, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
