23cf1d4b56e8946c7def30b1f3e64b7a3145bb9e
Uman, Howard <human@cvent.com>
2024-12-17 11:45:18 -0500

    2024 prep day 17

--- advent2024/src/main/java/com/unhuman/adventofcode2024/Advent2024.java
+++ advent2024/src/main/java/com/unhuman/adventofcode2024/Advent2024.java
@@ -24,7 +24,7 @@ public class Advent2024
 //        new Day14().process();
 //        new Day15().process();
         new Day16().process();
-//        new Day17().process();
+        new Day17().process();
 //        new Day18().process();
 //        new Day19().process();
 //        new Day20().process();
---
+++ advent2024/src/test/java/com/unhuman/adventofcode2024/Day17Test.java
@@ -0,0 +1,32 @@
+package com.unhuman.adventofcode2024;
+
+import com.unhuman.adventofcode.aoc_framework.InputParser;
+import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day17Test {
+    // data must be at least 2 lines - add \n for single line data
+    private static final String DATA =
+            "\n";
+
+    static InputParser getDay(String data) {
+        return new Day17(data);
+    }
+
+    @Test
+    public void test1() {
+        String data = DATA;
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test2() {
+        String data = DATA;
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0L, day.processInput2(groups[0], groups[1]));
+    }
+}
