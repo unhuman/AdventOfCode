7db9d8c5378859e92235b4a5b5a13bb2822b284b
Howard Uman <unhuman@gmail.com>
2024-12-21 17:51:34 -0500

    2024 Day 21 pt 1 tests pass, data does not

--- advent2024/src/main/java/com/unhuman/adventofcode2024/Day21.java
+++ advent2024/src/main/java/com/unhuman/adventofcode2024/Day21.java
@@ -4,9 +4,17 @@ import com.unhuman.adventofcode.aoc_framework.InputParser;
 import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
 import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
+import com.unhuman.adventofcode.aoc_framework.utility.Matrix;
+import com.unhuman.adventofcode.aoc_framework.utility.PointHelper;
+
+import javax.swing.text.Position;
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Day21 extends InputParser {
-    private static final String regex1 = null;
+    private static final String regex1 = "(.)";
     private static final String regex2 = null;
 
     public Day21() {
@@ -17,34 +25,174 @@ public class Day21 extends InputParser {
         super(filename, regex1, regex2);
     }
 
+    // 130788 too low
+    // 134700 too high
+
     @Override
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
+        KeyPad doorway = new KeyPad("789\n456\n123\n 0A");
+        KeyPad robot1 = new KeyPad(" ^A\n<v>");
+        KeyPad robot2 = new KeyPad(" ^A\n<v>");
+        KeyPad human  = new KeyPad(" ^A\n<v>");
+
+        List<KeyPad> keypads = Arrays.asList(doorway, robot1, robot2);
+
         // easier to assume there's only one group
+        Long result = 0L;
         GroupItem group0 = configGroup.get(0);
         for (ItemLine line : group0) {
-            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-//                char value = line.getChar(itemNum);
-//                String value = line.getString(itemNum);
-//                Long value = line.getLong(itemNum);
+            for (KeyPad keypad: keypads) {
+                keypad.resetFinger();
             }
+            String command = "";
+            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
+                command = command + line.getChar(itemNum);
+            }
+
+            String data = command;
+            for (KeyPad keypad: keypads) {
+                data = keypad.processCommand(data);
+            }
+
+            long commandValue = Long.parseLong(command.substring(0, command.length() - 1));
+            System.out.println("Command: " + command + " Length: " + data.length() + " * value: " + commandValue +
+                    " = " + data.length() * commandValue);
+            result += data.length() * commandValue;
+            System.out.println("Running total : " + result);
         }
 
-        // Here's code for a 2nd group, if needed
-//        GroupItem group1 = configGroup1.get(0);
-//        for (ItemLine line : group1) {
-//            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-////                char value = line.getChar(itemNum);
-////                String value = line.getString(itemNum);
-////                Long value = line.getLong(itemNum);
-//            }
-//        }
-
-
-        return 1;
+        return result;
     }
 
     @Override
     public Object processInput2(ConfigGroup configGroup, ConfigGroup configGroup1) {
         return 2;
+    }
+
+    public static class KeyPad {
+        Matrix matrix;
+        Point finger;
+        Point deadSpot;
+
+        KeyPad(String buttons) {
+            String[] lines = buttons.split("\\n");
+            matrix = new Matrix(lines[0].length(), lines.length, Matrix.DataType.CHARACTER);
+            for (int y = 0; y < lines.length; y++) {
+                for (int x = 0; x < lines[y].length(); x++) {
+                    Character button = lines[y].charAt(x);
+                    matrix.setValue(x, y, button);
+                    if (button.equals('A')) {
+                        finger = new Point(x, y);
+                    }
+                }
+            }
+            deadSpot = matrix.getCharacterLocations(' ').get(0);
+        }
+
+        void resetFinger() {
+            finger = matrix.getCharacterLocations('A').get(0);
+        }
+
+        boolean canWalk(Point start, Matrix.Direction direction, int steps) {
+            if (steps == 0) {
+                return false;
+            }
+            Point testStep = start;
+            for (int i = 0; i < steps; i++) {
+                testStep = PointHelper.addPoints(testStep, direction.getDirection());
+                if (deadSpot.equals(testStep)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        protected String moveTo(Character desiredButton) {
+            Point buttonLocation = matrix.getCharacterLocations(desiredButton).get(0);
+            Point delta = PointHelper.subtract(buttonLocation, finger);
+
+            int leftSteps = (delta.x < 0) ? Math.abs(delta.x) : 0;
+            int rightSteps = (delta.x > 0) ? delta.x : 0;
+
+            int upSteps = (delta.y < 0) ? Math.abs(delta.y) : 0;
+            int downSteps = (delta.y > 0) ? delta.y : 0;
+
+            // navigate to the button
+            StringBuilder sb = new StringBuilder();
+
+            while (leftSteps + rightSteps + upSteps + downSteps > 0) {
+                if (canWalk(finger, Matrix.Direction.LEFT, leftSteps)) {
+                    for (int i = 0; i < leftSteps; i++) {
+                        sb.append('<');
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.LEFT.getDirection());
+                    }
+                    leftSteps = 0;
+                } else if (canWalk(finger, Matrix.Direction.RIGHT, rightSteps)) {
+                    for (int i = 0; i < rightSteps; i++) {
+                        sb.append('>');
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.RIGHT.getDirection());
+                    }
+                    rightSteps = 0;
+                } else if (canWalk(finger, Matrix.Direction.UP, upSteps)) {
+                    for (int i = 0; i < upSteps; i++) {
+                        sb.append('^');
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.UP.getDirection());
+                    }
+                    upSteps = 0;
+                } else if (canWalk(finger, Matrix.Direction.DOWN, downSteps)) {
+                    for (int i = 0; i < downSteps; i++) {
+                        sb.append('v');
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.DOWN.getDirection());
+                    }
+                    downSteps = 0;
+                }
+            }
+
+            // press the button
+            sb.append('A');
+
+            return sb.toString();
+        }
+
+        String processCommand(String command) {
+            resetFinger();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < command.length(); i++) {
+                Character desiredButton = command.charAt(i);
+                sb.append(moveTo(desiredButton));
+            }
+            System.out.println(sb.toString() + " " + sb.toString().length());
+            return sb.toString();
+        }
+
+        String followDirections(String command) {
+            resetFinger();
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < command.length(); i++) {
+                switch (command.charAt(i)) {
+                    case '<':
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.LEFT.getDirection());
+                        break;
+                    case '>':
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.RIGHT.getDirection());
+                        break;
+                    case '^':
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.UP.getDirection());
+                        break;
+                    case 'v':
+                        finger = PointHelper.addPoints(finger, Matrix.Direction.DOWN.getDirection());
+                        break;
+                    case 'A':
+                        sb.append(matrix.getValue(finger));
+                }
+
+                if (matrix.getValue(finger) == ' ') {
+                    System.out.println("PANIC at index" + i);
+                }
+            }
+            return sb.toString();
+        }
     }
 }
--- advent2024/src/test/java/com/unhuman/adventofcode2024/Day21Test.java
+++ advent2024/src/test/java/com/unhuman/adventofcode2024/Day21Test.java
@@ -2,6 +2,7 @@ package com.unhuman.adventofcode2024;
 
 import com.unhuman.adventofcode.aoc_framework.InputParser;
 import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -15,12 +16,121 @@ public class Day21Test {
     }
 
     @Test
+    public void keypadTest1() {
+        Day21.KeyPad doorway = new Day21.KeyPad("789\n456\n123\n 0A");
+        String answer = doorway.followDirections("<A^A>^^AvvvA");
+        Assertions.assertEquals("029A", answer);
+    }
+
+    @Test
+    public void keypadTest2() {
+        Day21.KeyPad robot = new Day21.KeyPad(" ^A\n<v>");
+        String answer = robot.followDirections("v<<A>>^A<A>AvA<^AA>A<vAAA>^A");
+        Assertions.assertEquals("<A^A>^^AvvvA", answer);
+    }
+
+    @Test
+    public void keypadTest3() {
+        Day21.KeyPad doorway = new Day21.KeyPad("789\n456\n123\n 0A");
+        String program = doorway.processCommand("379A");
+        String answer = doorway.followDirections(program);
+        Assertions.assertEquals("379A", answer);
+
+        Day21.KeyPad robot = new Day21.KeyPad(" ^A\n<v>");
+        String robotDirections = robot.processCommand(program);
+        String robotCommand = robot.followDirections(robotDirections);
+        Assertions.assertEquals(program, robotCommand);
+
+        Day21.KeyPad robot2 = new Day21.KeyPad(" ^A\n<v>");
+        String robot2Directions = robot.processCommand(robotDirections);
+        String robot2Command = robot.followDirections(robot2Directions);
+        Assertions.assertEquals(robot2Command, robotDirections);
+
+        // Go back....
+        String output1 = robot.followDirections(robot2Command);
+        String output2 = doorway.followDirections(output1);
+        Assertions.assertEquals("379A", output2);
+    }
+
+    @Test
+    public void keypadTest4() {
+        Day21.KeyPad doorway = new Day21.KeyPad("789\n456\n123\n 0A");
+        Day21.KeyPad robot = new Day21.KeyPad(" ^A\n<v>");
+        Day21.KeyPad robot2 = new Day21.KeyPad(" ^A\n<v>");
+        //final String command = "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A";
+        final String command = "v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A";
+        System.out.println("Robot 2 Following: " + command);
+        String robot2Results = robot2.followDirections(command);
+        System.out.println("Robot 1 Following: " + robot2Results);
+        String robotResults = robot.followDirections(robot2Results);
+        System.out.println("Doorway Following: " + robotResults);
+        String doorwayResults = doorway.followDirections(robotResults);
+        System.out.println("Doorway Result: " + doorwayResults);
+        Assertions.assertEquals("379A", doorwayResults);
+
+        // Go back....
+        System.out.println("Doorway Processing: " + doorwayResults);
+        String output1 = doorway.processCommand(doorwayResults);
+        Assertions.assertEquals(robotResults, output1);
+        System.out.println("Robot 2 Processing: " + output1);
+        String output2 = robot2.processCommand(output1);
+//        Assertions.assertEquals(robot2Results, output2);
+        System.out.println("Robot 1 Processing: " + output2);
+        String output = robot.processCommand(output2);
+        System.out.println("Robot 1 Output: " + output);
+        Assertions.assertEquals(command, output);
+    }
+
+    @Test
+    public void keypadTest5() {
+        Day21.KeyPad doorway = new Day21.KeyPad("789\n456\n123\n 0A");
+        Day21.KeyPad robot = new Day21.KeyPad(" ^A\n<v>");
+        Day21.KeyPad robot2 = new Day21.KeyPad(" ^A\n<v>");
+        final String command = "v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A";
+//      final String command = "v<<A^>>AvA^Av<<A^>>AAv<A<A^>>AA<Av>AA^Av<A^>AA<A>Av<A<A^>>AAA<Av>A^A";
+        System.out.println("Robot 2 Following: " + command);
+        String robot2Results = robot2.followDirections(command);
+        System.out.println("Robot 1 Following: " + robot2Results);
+        String robotResults = robot.followDirections(robot2Results);
+        System.out.println("Doorway Following: " + robotResults);
+        String doorwayResults = doorway.followDirections(robotResults);
+        System.out.println("Doorway Result: " + doorwayResults);
+        Assertions.assertEquals("379A", doorwayResults);
+
+        // Go back....
+        System.out.println("Doorway Processing: " + doorwayResults);
+        String output1 = doorway.processCommand(doorwayResults);
+        Assertions.assertEquals(robotResults, output1);
+        System.out.println("Robot 2 Processing: " + output1);
+        String output2 = robot2.processCommand(output1);
+        Assertions.assertEquals(robot2Results, output2);
+        System.out.println("Robot 1 Processing: " + output2);
+        String output = robot.processCommand(output2);
+        System.out.println("Robot 1 Output: " + output);
+        Assertions.assertEquals(command, output);
+    }
+
+    @Test
     public void test1() {
-        String data = DATA;
+        String data = "029A\n" +
+                "980A\n" +
+                "179A\n" +
+                "456A\n" +
+                "379A\n";
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(126384L, day.processInput1(groups[0], groups[1]));
     }
+
+    @Test
+    public void test1a() {
+        String data =
+                "379A\n";
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(24256L, day.processInput1(groups[0], groups[1]));
+    }
+
 
     @Test
     public void test2() {
