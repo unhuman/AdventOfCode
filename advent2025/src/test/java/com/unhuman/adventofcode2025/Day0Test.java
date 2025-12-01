4bea1c198475fabcbf49d0b24955d7e41873ca44
Howard Uman <unhuman@gmail.com>
2025-12-01 16:50:40 -0500

    prep 2025

---
+++ advent2025/src/test/java/com/unhuman/adventofcode2025/Day0Test.java
@@ -0,0 +1,33 @@
+package com.unhuman.adventofcode2025;
+
+import com.unhuman.adventofcode.aoc_framework.InputParser;
+import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day0Test {
+    // data must be at least 2 lines - add \n for single line data
+    private static final String DATA = "" +
+            "\n";
+
+    static InputParser getDay(String data) {
+        return null;
+        //return new DayX(data);
+    }
+
+    @Test
+    public void test1() {
+        String data = DATA;
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0, day.processInput1(groups[0], groups[1]));
+    }
+
+    @Test
+    public void test2() {
+        String data = DATA;
+        InputParser day = getDay(data);
+        ConfigGroup[] groups = day.parseFiles();
+        Assertions.assertEquals(0, day.processInput2(groups[0], groups[1]));
+    }
+}
