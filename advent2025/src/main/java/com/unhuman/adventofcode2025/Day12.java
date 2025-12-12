bb2a4c0150118f87a1e27169dc1b65874e106065
Uman, Howard <human@cvent.com>
2025-12-12 16:06:48 -0500

    2025 day 12 - dirty part one doesn't pass test, but works

--- advent2025/src/main/java/com/unhuman/adventofcode2025/Day12.java
+++ advent2025/src/main/java/com/unhuman/adventofcode2025/Day12.java
@@ -4,32 +4,91 @@ import com.unhuman.adventofcode.aoc_framework.InputParser;
 import com.unhuman.adventofcode.aoc_framework.representation.ConfigGroup;
 import com.unhuman.adventofcode.aoc_framework.representation.GroupItem;
 import com.unhuman.adventofcode.aoc_framework.representation.ItemLine;
+import com.unhuman.adventofcode.aoc_framework.utility.Matrix;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day12 extends InputParser {
-    private static final String REGEX_1 = "";
-    private static final String REGEX_2 = null;
+    // Note: Built in parsing does not work for this solution
+    private static final String MATCH_ALL = "(.*)";
+    private static final String REGEX_1 = "(\\d+:)|([#.]*)";
+    private static final String REGEX_2 = "(\\d+)[x: ]?";
 
     public Day12() {
-        super(2025, 12, REGEX_1, REGEX_2);
+        super(2025, 12, MATCH_ALL, MATCH_ALL);
     }
 
     public Day12(String filename) {
-        super(filename, REGEX_1, REGEX_2);
+        super(filename, MATCH_ALL, MATCH_ALL);
     }
 
     @Override
     public Object processInput1(ConfigGroup configGroup, ConfigGroup configGroup1) {
+        // THe built in parsing is busted
+        configGroup.addAll(configGroup1);
+        configGroup1 = new ConfigGroup();
+        configGroup1.add(configGroup.removeLast());
+
+        Pattern shapePattern = Pattern.compile(REGEX_1);
+        Map<Integer, List<Matrix>> shapes = new HashMap<>();
+
         // easier to assume there's only one group
-        GroupItem group0 = configGroup.getFirst();
-        for (ItemLine line : group0) {
-            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
-//                char value = line.getChar(itemNum);
-//                String value = line.getString(itemNum);
-//                Long value = line.getLong(itemNum);
+        for (GroupItem group: configGroup) {
+            List<String> currentShape = new ArrayList<>();
+            Integer number = null;
+            for (ItemLine line : group) {
+                Matcher shapeMatcher = shapePattern.matcher(line.toString());
+                if (shapeMatcher.find()) {
+                    if (shapeMatcher.group(1) != null && shapeMatcher.group(1).endsWith(":")) {
+                        number = Integer.parseInt(shapeMatcher.group(1).substring(0, shapeMatcher.group(1).indexOf(':')));
+                        shapes.put(number, new ArrayList<>());
+                    } else {
+                        currentShape.add(shapeMatcher.group(0));
+                    }
+                }
+            }
+
+            Matrix matrix = new Matrix(currentShape);
+            for (int i = 0; i < 4; i++) {
+                shapes.get(number).add(matrix);
+                shapes.get(number).add(Matrix.flip(matrix, Matrix.Direction.Flip.HORIZONTAL));
+                // we don't need to flip two ways:
+                // shapes.get(number).add(Matrix.flip(matrix, Matrix.Direction.Flip.VERTICAL));
+                matrix = Matrix.rotate(matrix, Matrix.Direction.Rotation.CLOCKWISE);
             }
         }
 
-        // Here's code for a 2nd group, if needed
+        Pattern fillPattern = Pattern.compile(REGEX_2);
+
+        long count = 0;
+        for (ItemLine line : configGroup1.getFirst()) {
+            List<Integer> items = new ArrayList<>();
+            Matcher matcher = fillPattern.matcher(line.toString());
+            while (matcher.find()) {
+                items.add(Integer.parseInt(matcher.group(1)));
+            }
+
+            int totalSquares = 0;
+            int xSize = items.get(0);
+            int ySize = items.get(1);
+
+            int squaresCapacity = xSize / 3 * ySize / 3;
+
+            for (int i = 2; i < items.size(); i++) {
+                totalSquares += items.get(i);
+            }
+
+            if (totalSquares <= squaresCapacity) {
+                ++count;
+            }
+        }
+
+            // Here's code for a 2nd group, if needed
 //        GroupItem group1 = configGroup1.getFirst();
 //        for (ItemLine line : group1) {
 //            for (int itemNum = 0; itemNum < line.size(); itemNum++) {
@@ -40,7 +99,7 @@ public class Day12 extends InputParser {
 //        }
 
 
-        return 1L;
+        return count;
     }
 
     @Override
--- advent2025/src/test/java/com/unhuman/adventofcode2025/Day12Test.java
+++ advent2025/src/test/java/com/unhuman/adventofcode2025/Day12Test.java
@@ -7,12 +7,50 @@ import org.junit.jupiter.api.Test;
 
 public class Day12Test {
     // data must be at least 2 lines - add \n for single line data
-    private static final String DATA = "" +
+    private static final String DATA =
+            "99:\n" +
+            "#..\n" +
+            "...\n" +
+            "...\n" +
+            "\n" +
+            "0:\n" +
+            "###\n" +
+            "##.\n" +
+            "##.\n" +
+            "\n" +
+            "1:\n" +
+            "###\n" +
+            "##.\n" +
+            ".##\n" +
+            "\n" +
+            "2:\n" +
+            ".##\n" +
+            "###\n" +
+            "##.\n" +
+            "\n" +
+            "3:\n" +
+            "##.\n" +
+            "###\n" +
+            "##.\n" +
+            "\n" +
+            "4:\n" +
+            "###\n" +
+            "#..\n" +
+            "###\n" +
+            "\n" +
+            "5:\n" +
+            "###\n" +
+            ".#.\n" +
+            "###\n" +
+            "\n" +
+            "4x4: 0 0 0 0 2 0\n" +
+            "12x5: 1 0 1 0 2 2\n" +
+            "12x5: 1 0 1 0 3 2\n" +
             "\n";
 
     static InputParser getDay(String data) {
-        return null;
-        //return new DayX(data);
+//        return null;
+        return new Day12(data);
     }
 
     @Test
@@ -20,7 +58,7 @@ public class Day12Test {
         String data = DATA;
         InputParser day = getDay(data);
         ConfigGroup[] groups = day.parseFiles();
-        Assertions.assertEquals(0L, day.processInput1(groups[0], groups[1]));
+        Assertions.assertEquals(2L, day.processInput1(groups[0], groups[1]));
     }
 
     @Test
